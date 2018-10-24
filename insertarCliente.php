dda122c883a0b81a901f7d58a0981c81b67d53b9
Valeria Rodriguez <valery-rod@hotmail.com>
2018-10-23 20:16:24 -0500

    add php insert

---
+++ insertarCliente.php
@@ -0,0 +1,43 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Inserción de Datos </title> 
+</head>
+<body>
+<form>
+   <CENTER><td colspan="2" align="center"><h2>ADICIONANDO CLIENTES</h2></td></center>
+   <table align="center" width="255" cellspacing="2" cellpadding="2" border="0">
+</form>
+<form>
+ <LEFT><td colspan="2" align="left"><h3> Factura Insertada</h3></td></LEFT>
+<table align="left" width="100" cellspacing="2" cellpadding="2" border="0">
+</form>
+
+<?php
+	//campos
+   $v_cod_cliente=$_GET["codigo_cliente"];
+   $v_nom_cliente="'".$_GET["nombre_cliente"]."'";
+   $v_cod_vendedor=$_GET["codigo_vendedor"];
+   $v_cod_ciudad=$_GET["codigo_ciudad"];
+   $v_cod_f_pago=$_GET["codigo_f_pago"];
+
+
+$conexion = pg_connect("host=localhost port=5432 dbname=VENTAS user=postgres password=123"); //conexion
+
+$sql ="INSERT INTO cliente VALUES($v_cod_cliente, $v_nom_cliente, $v_cod_vendedor, $v_cod_ciudad, $v_cod_f_pago)";
+
+$result = pg_Exec($conexion, $sql);
+
+if ($result) {
+	echo "insercion exitosa";
+}else{
+	echo "no se pudo insertar los datos. Intente nuevamente";
+}
+echo"<BR>";
+?>
+<a href="insertarDatos.php">regresar</a>
+
+</form>
+</body>
+</html>
---
+++ insertarFactura.php
@@ -0,0 +1,40 @@
+
+<html>
+<head>
+	<title> Insercion de Datos </title> 
+</head>
+<body>
+<form>
+   <CENTER><td colspan="2" align="center"><h2>ADICIONANDO FACTURAS</h2></td></center>
+   <table align="center" width="255" cellspacing="2" cellpadding="2" border="0">
+</form>
+<form>
+ <LEFT><td colspan="2" align="left"><h3> Factura Insertada</h3></td></LEFT>
+<table align="left" width="100" cellspacing="2" cellpadding="2" border="0">
+</form>
+
+<?php
+	//campos
+   $v_cod_factura=$_GET["codigo_factura"];
+   $v_nom_cliente="'".$_GET["nombre_cliente"]."'";
+   $v_valor_factura=$_GET["valor_factura"];
+
+
+$conexion = pg_connect("host=localhost port=5432 dbname=Ventas user=postgres password=123"); //conexion
+
+$sql ="INSERT INTO factura VALUES($v_cod_factura, $v_nom_cliente, $v_valor_factura)";
+
+$result = pg_Exec($conexion, $sql);
+
+if ($result) {
+	echo "insercion exitosa";
+}else{
+	echo "no se pudo insertar los datos. Intente nuevamente";
+}
+echo"<BR>";
+?>
+<a href="insertarDatos.php">regresar</a>
+
+</form>
+</body>
+</html>
