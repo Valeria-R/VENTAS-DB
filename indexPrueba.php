24fcb86cbd82dadfb2735a2bbf223803a8b62790
Valeria Rodriguez <valery-rod@hotmail.com>
2018-10-31 08:26:54 -0500

    added new index

---
+++ indexPrueba.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" type="text/css" href="CSS1.css"> 
+</head>
+<body>
+  <div class="wrap">
+      <span class="decor"></span>
+      <nav>
+        <ul class="primary">
+          <li>
+            <a href="">Insertar</a>
+            <ul class="sub">
+              <li><a href="formularioVendedor.php">Vendedor</a></li>
+              <li><a href="formularioCiudad.php">Ciudad</a></li>
+              <li><a href="formularioFPago.php">Forma de pago</a></li>
+              <li><a href="formularioCliente.php">Cliente</a></li>
+              <li><a href="formularioFactura.php">Factura</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="">Consultar</a>
+            <ul class="sub">
+              <li><a href="consultarPrincipal.php">Generales</a></li>
+              <li><a href="ComprasClientes.php">Compras por Clientes</a></li>
+              <li><a href="VentasFPago.php">Ventas por forma de pago</a></li>
+              <li><a href="VentasVendedor.php">Ventas por vendedor</a></li>
+              <li><a href="VentasCiudad.php">Ventas por ciudad</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      </div>
+<br>
+<br>
+<br>
+    <img src="https://i1.wp.com/imagenessobre.com/wp-content/uploads/2017/05/bienvenido.png?resize=620%2C207" align="center" border="0">
+</body>
+</html>
